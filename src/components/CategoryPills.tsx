354c9abc0cdd741a25defb7c4ca113e5bdae81b9
Krishna <leelakrishna682@gmail.com>
2024-07-26 20:42:02 +0530

    Update CategoryPills.tsx

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,4 +1,4 @@
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronLeft, ChevronRight} from "lucide-react";
 import { Button } from "./Button";
 import { useEffect, useRef, useState } from "react";
 
