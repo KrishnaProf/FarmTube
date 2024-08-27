575e0a1544f36cb17dfbcb1cfd85ff3ad605a64c
Krishna <leelakrishna682@gmail.com>
2024-08-27 19:13:06 +0530

    Update CategoryPills.tsx

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,7 +1,7 @@
 import {ChevronLeft, ChevronRight} from "lucide-react";
 import { Button} from "./Button";
-
 import { useEffect, useRef, useState} from "react";
+
 
 type CategoryPillProps = {
   categories: string[];
