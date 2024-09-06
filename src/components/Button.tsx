5429ec6aa70e00b91626fa22ba52edf95913aa81
Krishna <leelakrishna682@gmail.com>
2024-09-06 19:23:02 +0530

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
 import { VariantProps, cva } from "class-variance-authority";
 import { ComponentProps } from "react";
+
 import { twMerge } from "tailwind-merge";
 
 export const buttonStyles = cva(["transition-colors"], {
